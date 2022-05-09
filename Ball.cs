0ced353025010d40d14f7a8c5ca331e3f9de27bf
Michael Mathu <mathumichael17@gmail.com>
2022-05-09 12:39:45 +0300

    initial commit

---
+++ Ball.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Ball
+{
+    internal class Ball
+    {
+        private int size;
+        private int color;
+        private int thrown;
+
+        public Ball(int size, int color, int thrown)
+        {
+            this.size = size;
+            this.color = color;
+            this.thrown = thrown;
+        }
+
+        public int Pop()
+        {
+            size = 0;
+            return size;
+        }
+        public int Thrown()
+        {
+            if(size == 0)
+                this.thrown += 1;
+
+            return thrown;
+        }
+
+        public int Times()
+        {
+
+        }
+    }
+}
